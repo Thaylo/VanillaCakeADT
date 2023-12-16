6e00bf5cc812dd738ce65ce07461437ca9bc00fb
Thaylo de Freitas <thayloxavier@gmail.com>
2023-12-16 09:36:38 -0300

    Implements countElementsOnList_test

--- tests/test_List.cpp
+++ tests/test_List.cpp
@@ -43,6 +43,64 @@ TEST_F(ListTest, insertToFrontOfList_test)
     EXPECT_EQ(countElementsOnList(list), repetitions);
 }
 
+TEST_F(ListTest, elementAtList_empty_test)
+{
+    EXPECT_EQ(elementAtList(list, -1), (DataContainer *) NULL);
+    EXPECT_EQ(elementAtList(list, 0), (DataContainer *) NULL);
+    EXPECT_EQ(elementAtList(list, 1), (DataContainer *) NULL);
+    EXPECT_EQ(elementAtList(list, 10), (DataContainer *) NULL);
+}
+
+TEST_F(ListTest, elementAtList_nullList_test)
+{
+    EXPECT_EQ(elementAtList(NULL, -1), (DataContainer *) NULL);
+    EXPECT_EQ(elementAtList(NULL, 0), (DataContainer *) NULL);
+    EXPECT_EQ(elementAtList(NULL, 1), (DataContainer *) NULL);
+    EXPECT_EQ(elementAtList(NULL, 10), (DataContainer *) NULL);
+}
+
+TEST_F(ListTest, elementAtList_notEmpty_test)
+{
+    int numberOfElements = 5;
+    size_t size = numberOfElements * sizeof(float*);
+    float * data[5];
+    for(int i = 0; i < numberOfElements; ++i)
+    {
+         // DataContainer is responsible for deallocating those floats
+        data[i] = (float*) malloc(sizeof(float));
+
+        // Creating the source data as an array: 0.0, 2.0, 4.0, 6.0, 8.0
+        *(data[i]) = i*2;
+    }
+    
+    for(int i = 0; i < numberOfElements; ++i)
+    {
+        DataContainer * dataContainer = encapsulateDataOnDataContainer(
+            data[i],
+            sizeof(float),
+            free,
+            testHelper_floatPointerDisplayFunction);
+
+        // Storing the source data
+        int status = insertToFrontOfList(list, dataContainer);
+        EXPECT_EQ(status, SUCCESS);
+    }
+    
+    EXPECT_EQ(elementAtList(list, -1), (DataContainer *) NULL);
+    for(int i = 0; i < numberOfElements; ++i)
+    {
+        void * dataContainerData;
+        size_t dataContainerSize;
+
+        getDataOnDataContainer(elementAtList(list, i), &dataContainerData, &dataContainerSize);
+        
+        // Verifying against the sequence of values: 4.0, 3.0, 2.0, 1.0, 0.0
+        EXPECT_EQ(*(float*) dataContainerData, *(data[numberOfElements - (i + 1)]));
+    }
+
+    EXPECT_EQ(elementAtList(list, numberOfElements), (DataContainer *) NULL);
+}
+
 TEST_F(ListTest, removeFromFrontOfList_test)
 {
     int inclusions = 10;
@@ -122,61 +180,15 @@ TEST_F(ListTest, insertToFrontOfList_ordering_and_popFromFrontOfList_test)
 
 }
 
-TEST_F(ListTest, elementAtList_empty_test)
+TEST_F(ListTest, countElementsOnList_test)
 {
-    EXPECT_EQ(elementAtList(list, -1), (DataContainer *) NULL);
-    EXPECT_EQ(elementAtList(list, 0), (DataContainer *) NULL);
-    EXPECT_EQ(elementAtList(list, 1), (DataContainer *) NULL);
-    EXPECT_EQ(elementAtList(list, 10), (DataContainer *) NULL);
+    EXPECT_EQ(countElementsOnList(list), 0);
+    insertToFrontOfList(list, NULL);
+    EXPECT_EQ(countElementsOnList(list), 1);
+    insertToFrontOfList(list, NULL);
+    EXPECT_EQ(countElementsOnList(list), 2);
+    popFromFrontOfList(list);
+    EXPECT_EQ(countElementsOnList(list), 1);
+    popFromFrontOfList(list);
+    EXPECT_EQ(countElementsOnList(list), 0);
 }
-
-TEST_F(ListTest, elementAtList_nullList_test)
-{
-    EXPECT_EQ(elementAtList(NULL, -1), (DataContainer *) NULL);
-    EXPECT_EQ(elementAtList(NULL, 0), (DataContainer *) NULL);
-    EXPECT_EQ(elementAtList(NULL, 1), (DataContainer *) NULL);
-    EXPECT_EQ(elementAtList(NULL, 10), (DataContainer *) NULL);
-}
-
-TEST_F(ListTest, elementAtList_notEmpty_test)
-{
-    int numberOfElements = 5;
-    size_t size = numberOfElements * sizeof(float*);
-    float * data[5];
-    for(int i = 0; i < numberOfElements; ++i)
-    {
-         // DataContainer is responsible for deallocating those floats
-        data[i] = (float*) malloc(sizeof(float));
-
-        // Creating the source data as an array: 0.0, 2.0, 4.0, 6.0, 8.0
-        *(data[i]) = i*2;
-    }
-    
-    for(int i = 0; i < numberOfElements; ++i)
-    {
-        DataContainer * dataContainer = encapsulateDataOnDataContainer(
-            data[i],
-            sizeof(float),
-            free,
-            testHelper_floatPointerDisplayFunction);
-
-        // Storing the source data
-        int status = insertToFrontOfList(list, dataContainer);
-        EXPECT_EQ(status, SUCCESS);
-    }
-    
-    EXPECT_EQ(elementAtList(list, -1), (DataContainer *) NULL);
-    for(int i = 0; i < numberOfElements; ++i)
-    {
-        void * dataContainerData;
-        size_t dataContainerSize;
-
-        getDataOnDataContainer(elementAtList(list, i), &dataContainerData, &dataContainerSize);
-        
-        // Verifying against the sequence of values: 4.0, 3.0, 2.0, 1.0, 0.0
-        EXPECT_EQ(*(float*) dataContainerData, *(data[numberOfElements - (i + 1)]));
-    }
-
-    EXPECT_EQ(elementAtList(list, numberOfElements), (DataContainer *) NULL);
-}
-
